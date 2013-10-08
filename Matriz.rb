0e35856c904280383e64f90c436a30ad74fb6d07
Ayoze Elvira <alu0100696974@ull.edu.es>
2013-10-08 15:33:32 +0100

    1 version

---
+++ Matriz.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+
+
+a = [[1,2,3],[4,5,6],[7,8,9]]
+
+b = [[1,1,1],[2,2,2],[3,3,3]]
+
+c = [[0,0,0],[0,0,0],[0,0,0]]
+
+
+def suma(a,b,c)
+
+  
+  for i in (0..2)
+
+    for j in (0..2)
+
+      c[i][j] = a[i][j] + b[i][j]
+
+    end
+  end
+
+puts "La matriz SUMA es: #{c}"
+
+
+end
+
+
+puts "La matriz A es: #{a}"
+
+puts "La matriz B es: #{b}"
+
+puts suma(a,b,c)
