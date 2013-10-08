848dab70e15b9365134a06290b6424e9f826aa73
Bruno Bibbo <alu0100697063@ull.edu.es>
2013-10-08 15:33:55 +0100

    Version inicial multiplicacion

--- Matriz.rb
+++ Matriz.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 
-#conflicto 1
+
 a = [[1,2,3],[4,5,6],[7,8,9]]
 
 b = [[1,1,1],[2,2,2],[3,3,3]]
@@ -31,3 +31,17 @@ puts "La matriz A es: #{a}"
 puts "La matriz B es: #{b}"
 
 puts suma(a,b,c)
+
+d = [[0,0,0],[0,0,0],[0,0,0]]
+
+def mul(a, b)
+	for i in (0..2)
+		for j in (0..2)
+			for k in (0..2)
+				d[i][j] += a[i][k]*b[k][j]
+			end
+		end
+	end
+end
+
+puts d
